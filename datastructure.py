1bcba7e46fbd1992652fcaea731b7b8ae620e36b
Gloria Muema <gloriamuema98@gmail.com>
2025-09-16 16:34:11 +0300

    my project

---
+++ datastructure.py
@@ -0,0 +1,24 @@
+#List
+fruits = ["Mangoes","Apples","Bananas","Oranges","Pear"]
+print(fruits[2])
+
+#dictionary
+my_dict = { "name": "Mary", "age": 45 }
+print(f"Age: {my_dict["age"]}")
+
+#Tuples
+numbers = (1,2,4)
+#modification
+#numbers[1] = 8
+print(numbers)
+
+
+#Sets
+list = [1,2,3,5,4,6,7,8,4,5,6,7,9,3]
+set = set(list)
+print(set)
+
+
+my_list = input("Enter five values")
+my_set = set(my_list)
+print(my_set)
